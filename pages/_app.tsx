6fa4f979491084a511ebab6a776573a27f755143
Marco Budiongan <marcobdngn@gmail.com>
2022-05-04 18:39:14 +0800

    refactor: css styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
